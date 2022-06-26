9ee926bd86d7f162b85fd97ad770b6f541deb918
txxo <kisuat@hotmail.com>
2022-06-26 16:18:17 +0800

    update

--- src/components/Msg.svelte
+++ src/components/Msg.svelte
@@ -1,16 +1,15 @@
 <script>
-  export let item
-  import {  quintOut } from 'svelte/easing'
+  import { quintOut } from 'svelte/easing'
   import { del } from '../config/fb'
   import { scale } from 'svelte/transition'
   import moment from 'moment'
+  export let item
   function getTime(t) {
     return moment(t * 1000).format('MM-DD  HH:mm:ss')
   }
 </script>
 
 <li
-  
   on:dblclick={() => del(item.id)}
   in:scale={{ duration: 1000, opacity: 0, easing: quintOut }}
   out:scale={{ duration: 1000, opacity: 0, easing: quintOut }}
